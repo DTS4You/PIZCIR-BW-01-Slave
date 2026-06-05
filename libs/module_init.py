11d82d5a4dcdec024c00c28793deafce2da577b3
Norbert Schwarz <datentechnikschwarz@gmail.com>
2026-06-05 14:44:14 +0200

    Init

--- libs/module_init.py
+++ libs/module_init.py
@@ -59,7 +59,7 @@ class Global_WS2812:
     color_off           = (  0,  0,  0)
     color_on            = (100,100,100)
     color_dot           = ( 50, 50, 50)
-    color_blink_on      = (  0,200,  0)
+    color_blink_on      = (  0,150,  0)
     color_blink_off     = (  0, 10,  0)
 
 
